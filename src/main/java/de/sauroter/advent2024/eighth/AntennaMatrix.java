3369c323ffd8f644aac97ea2b43f6f28282d82f4
Georg Held <georg.held@hivemq.com>
2024-12-08 10:36:23 +0100

    Advent 8th.

---
+++ src/main/java/de/sauroter/advent2024/eighth/AntennaMatrix.java
@@ -0,0 +1,129 @@
+package de.sauroter.advent2024.eighth;
+
+import java.util.*;
+
+public class AntennaMatrix {
+    public record Position(int row, int col) {
+        public Position vectorTo(final Position b) {
+            return new Position(b.row - this.row, b.col - this.col);
+        }
+
+        public Position negative() {
+            return new Position(-this.row, -this.col);
+        }
+
+        public Position add(final Position b) {
+            return new Position(this.row + b.row, this.col + b.col);
+        }
+    }
+
+    private final int height;
+    private final int width;
+    private final Set<Position> antinodes = new HashSet<>();
+    private final Map<Character, List<Position>> antennas = new HashMap<>();
+
+    public AntennaMatrix(final Scanner scanner) {
+
+        final List<String> lines = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
+        }
+
+        var width = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            final var line = lines.get(i).toCharArray();
+            width = line.length;
+            for (int j = 0; j < line.length; j++) {
+                final var c = line[j];
+                if (c != '.') {
+                    final var positions = antennas.computeIfAbsent(c, (ignored) -> new ArrayList<>());
+                    positions.add(new Position(i, j));
+                }
+            }
+        }
+        this.height = lines.size();
+        this.width = width;
+    }
+
+    private boolean isInBounds(final Position position) {
+        return position.row >= 0 && position.row < height && position.col >= 0 && position.col < width;
+    }
+
+    public Set<Position> calculateAntidnodes() {
+        for (final List<Position> antennaPositions : antennas.values()) {
+            if (antennaPositions.size() < 2) {
+                continue;
+            }
+            for (int i = 0; i < antennaPositions.size() - 1; i++) {
+                final var firstPosition = antennaPositions.get(i);
+                for (int j = 1; j < antennaPositions.size(); j++) {
+                    final var secondPosition = antennaPositions.get(j);
+                    if (Objects.equals(firstPosition, secondPosition)) {
+                        continue;
+                    }
+
+                    final var vectorTo = firstPosition.vectorTo(secondPosition);
+                    final var firstAntinode = firstPosition.add(vectorTo.negative());
+                    final var secondAntinode = secondPosition.add(vectorTo);
+
+                    if (isInBounds(firstAntinode)) {
+                        antinodes.add(firstAntinode);
+                    }
+
+                    if (isInBounds(secondAntinode)) {
+                        antinodes.add(secondAntinode);
+                    }
+                }
+            }
+        }
+
+        return antinodes;
+    }
+
+    public Set<Position> calculateAntidnodesWithHarmonics() {
+        for (final List<Position> antennaPositions : antennas.values()) {
+            if (antennaPositions.size() < 2) {
+                continue;
+            }
+            for (int i = 0; i < antennaPositions.size() - 1; i++) {
+                final var firstPosition = antennaPositions.get(i);
+                antinodes.add(firstPosition);
+                for (int j = 1; j < antennaPositions.size(); j++) {
+                    final var secondPosition = antennaPositions.get(j);
+                    antinodes.add(secondPosition);
+                    if (Objects.equals(firstPosition, secondPosition)) {
+                        continue;
+                    }
+                    final var vectorTo = firstPosition.vectorTo(secondPosition);
+
+                    Position left = firstPosition;
+                    while (isInBounds(left = left.add(vectorTo.negative()))) {
+                        antinodes.add(left);
+                    }
+
+                    Position right = secondPosition;
+                    while (isInBounds(right = right.add(vectorTo))) {
+                        antinodes.add(right);
+                    }
+                }
+            }
+        }
+        return antinodes;
+    }
+
+    public void print() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                final var position = new Position(i, j);
+                if (antinodes.contains(position)) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
---
+++ src/main/java/de/sauroter/advent2024/eighth/two/Main.java
@@ -0,0 +1,32 @@
+package de.sauroter.advent2024.eighth.two;
+
+import de.sauroter.advent2024.eighth.AntennaMatrix;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        final var inputStream = Main.class.getClassLoader().getResourceAsStream("eighth/input.txt");
+        final var scanner = new Scanner(Objects.requireNonNull(inputStream));
+        final var main = new Main();
+
+        main.loop(scanner);
+
+        scanner.close();
+    }
+
+    void loop(final Scanner scanner) {
+        final var antennaMatrix = new AntennaMatrix(scanner);
+
+        final var positions = antennaMatrix.calculateAntidnodesWithHarmonics();
+
+        System.out.println(positions);
+
+        antennaMatrix.print();
+        System.out.println(positions.size());
+    }
+
+}
